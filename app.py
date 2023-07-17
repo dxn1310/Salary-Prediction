a3b539a9c5b5e290e9754e83d765ba79586a1025
Dhanish Siddharth <78961654+dxn1310@users.noreply.github.com>
2023-07-17 20:27:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
-from predict_page import show_predict_page
+# from predict_page import show_predict_page
 
-show_predict_page()
+# show_predict_page()
+
+st.markdown("<h1 style='text-align: center; color: white;'>Software Developer Salary Prediction</h1>", unsafe_allow_html=True)
+st.write("""### We need some information to predict the salary""")
